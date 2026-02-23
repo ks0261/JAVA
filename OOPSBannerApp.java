5edb1c79f2365b398cfb5e20f9dc68fe124f49bf
ks0261 <ks0261@srmist.edu.in>
2026-02-23 15:53:34 +0530

    feature/UC4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,18 @@
-class UC3{
+import java.util.*;
+class UC4 {
     public static void main(String args[]){
-        System.out.println(String.join(" ","   ***   ", "    ***   ", "  ******  ", "    *****"));
-        System.out.println(String.join(" "," **   ** ", "  **   ** ", " **    ** ", "  **"));
-        System.out.println(String.join(" ","**     **", " **     **", " **     **", " **"));
-        System.out.println(String.join(" ","**     **", " **     **", " **    ** ", "  **"));
-        System.out.println(String.join(" ","**     **", " **     **", " ******   ", "    ***"));
-        System.out.println(String.join(" ","**     **", " **     **", " **       ", "       **"));
-        System.out.println(String.join(" ","**     **", " **     **", " **       ", "         **"));
-        System.out.println(String.join(" "," **   ** ", "  **   ** ", " **       ", "        **"));
-        System.out.println(String.join(" ","   ***   ", "    ***   ", " **       ", "   *****"));
+        Scanner sc=new Scanner(System.in);
+        String lines[] = new String[7];
+        lines[0] = (String.join(" ","   ***   ", "    ***   ", "  ******  ", "    *****"));
+        lines[1] = (String.join(" "," **   ** ", "  **   ** ", " **    ** ", "  **"));
+        lines[2] = (String.join(" ","**     **", " **     **", " **     **", " **"));
+        lines[3] = (String.join(" ","**     **", " **     **", " **    ** ", "  *****"));
+        lines[4] = (String.join(" ","**     **", " **     **", " ******   ", "      **"));
+        lines[5] = (String.join(" "," **   ** ", "  **   ** ", " **       ", " **   **"));
+        lines[6] = (String.join(" ","   ***   ", "    ***   ", " **       ", "  *****"));
+
+        for(int i=0;i<lines.length;i++){
+            System.out.println(lines[i]);
+        }
     }
 }
