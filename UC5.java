a85b9589bab94f820d1025c99c4c404a0003acdf
ks0261 <ks0261@srmist.edu.in>
2026-02-23 15:57:02 +0530

    feature/UC6

--- UC5.java
+++ UC5.java
@@ -1,17 +1,59 @@
 import java.util.*;
-public class UC5 {
+class UC6 {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        String lines[] = {
-        (String.join(" ","   ***   ", "    ***   ", "  ******  ", "    *****")),
-        (String.join(" "," **   ** ", "  **   ** ", " **    ** ", "  **")),
-        (String.join(" ","**     **", " **     **", " **     **", " **")),
-        (String.join(" ","**     **", " **     **", " **    ** ", "  *****")),
-        (String.join(" ","**     **", " **     **", " ******   ", "      **")),
-        (String.join(" "," **   ** ", "  **   ** ", " **       ", "      **")),
-        (String.join(" ","   ***   ", "    ***   ", " **       ", "  *****"))};
-        for(int i=0;i<lines.length;i++){
-            System.out.println(lines[i]);
+        Scanner sc = new Scanner(System.in);
+        UC6 ob=new UC6();
+        String s1[] = ob.getOPattern();
+        String s2[] = ob.getOPattern();
+        String s3[] = ob.getPPattern();
+        String s4[] = ob.getSPattern();
+        for(int i=0;i<9;i++){
+            System.out.print(s1[i]+" "+s2[i]+" "+s3[i]+" "+s4[i]);
+            System.out.println();
         }
     }
+    
+    public String[] getOPattern()
+    {
+
+        String lines[] ={("   ***   "),
+                         (" **   ** "),
+                         ("**     **"),
+                         ("**     **"),
+                         ("**     **"),
+                         ("**     **"),
+                         ("**     **"),
+                         (" **   ** "),
+                         ("   ***   "),
+        }; 
+        return lines;
+    }
+
+    public String[] getPPattern()
+    {
+        String lines[]={("  ******  "),
+                        (" **    ** "),
+                        (" **     **"),
+                        (" **    ** "),
+                        (" ******   "),
+                        (" **       "),
+                        (" **       "),
+                        (" **       " ),
+                        (" **       ")};
+        return lines;
+    }
+
+    public String[] getSPattern(){
+        String lines[]={("    *****"),
+                        ("  **"),
+                        (" **"),
+                        ("  **"),
+                        ("    ***"),
+                        ("       **"),
+                        ("         **"),
+                        ("        **"),
+                        ("   *****")};
+        return lines;
+    }
+
 }
