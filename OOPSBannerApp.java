a5244159b3c747d8129757f29046c8a2d313a8b5
ks0261 <ks0261@srmist.edu.in>
2026-02-23 15:48:33 +0530

    feature/UC3=PrintString

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,13 @@
-class UC2 {
-    public static void main(String[] args) {
-        System.out.println("   ***   "+"    ***   "+" ******   "+"    *****");
-        System.out.println(" **   ** "+"  **   ** "+" **    **  "+" **");
-        System.out.println("**     **"+" **     **"+" **     **"+" **");
-        System.out.println("**     **"+" **     **"+" **    ** "+"  **");
-        System.out.println("**     **"+" **     **"+" ******   "+"    ***");
-        System.out.println("**     **"+" **     **"+" **       "+"       **");
-        System.out.println("**     **"+" **     **"+" **       "+"         **");
-        System.out.println(" **   ** "+"  **   ** "+" **       "+"        **");
-        System.out.println("   ***   "+"    ***  "+"  **       "+"   *****");
+class UC3{
+    public static void main(String args[]){
+        System.out.println(String.join(" ","   ***   ", "    ***   ", "  ******  ", "    *****"));
+        System.out.println(String.join(" "," **   ** ", "  **   ** ", " **    ** ", "  **"));
+        System.out.println(String.join(" ","**     **", " **     **", " **     **", " **"));
+        System.out.println(String.join(" ","**     **", " **     **", " **    ** ", "  **"));
+        System.out.println(String.join(" ","**     **", " **     **", " ******   ", "    ***"));
+        System.out.println(String.join(" ","**     **", " **     **", " **       ", "       **"));
+        System.out.println(String.join(" ","**     **", " **     **", " **       ", "         **"));
+        System.out.println(String.join(" "," **   ** ", "  **   ** ", " **       ", "        **"));
+        System.out.println(String.join(" ","   ***   ", "    ***   ", " **       ", "   *****"));
     }
-    
 }
