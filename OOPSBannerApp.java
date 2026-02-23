5dea5e08a86762d3d4189015c11cfbf759158e31
ks0261 <ks0261@srmist.edu.in>
2026-02-23 15:35:52 +0530

    Printing OOPS Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,14 @@
-public class OOPSBannerApp {
+class UC2 {
     public static void main(String[] args) {
-        System.err.println("OOPS");
+        System.out.println("   ***   "+"    ***   "+" ******   "+"    *****");
+        System.out.println(" **   ** "+"  **   ** "+" **    **  "+" **");
+        System.out.println("**     **"+" **     **"+" **     **"+" **");
+        System.out.println("**     **"+" **     **"+" **    ** "+"  **");
+        System.out.println("**     **"+" **     **"+" ******   "+"    ***");
+        System.out.println("**     **"+" **     **"+" **       "+"       **");
+        System.out.println("**     **"+" **     **"+" **       "+"         **");
+        System.out.println(" **   ** "+"  **   ** "+" **       "+"        **");
+        System.out.println("   ***   "+"    ***  "+"  **       "+"   *****");
     }
+    
 }
