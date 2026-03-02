d156e4cb60b76aa252a5fdcf6417009ddcdfbaf3
ks0261 <ks0261@srmist.edu.in>
2026-03-02 21:34:51 +0530

    GetPattern

---
+++ OOPSBannerApp.java
@@ -0,0 +1,67 @@
+public class OOPSBannerApp {
+    static class CharacterPatternMap{
+        char character;
+        String pattern[];
+
+    public CharacterPatternMap(char character,String pattern[]){
+        this.character = character;
+        this.pattern = pattern;
+    }
+    public char getCharacter(){
+        return character;
+    }
+    public String[] getPattern(){
+        return pattern;
+    }
+}
+
+public static void main(String args[]){
+    CharacterPatternMap letters[] = {
+        new CharacterPatternMap('O', new String[]{
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
+        }),
+        new CharacterPatternMap('O', new String[]{
+                "   ***   ",
+                " **   ** ",
+                "**     **",
+                "**     **",
+                "**     **",
+                " **   ** ",
+                "   ***   "
+        }),
+        new CharacterPatternMap('P', new String[]{
+                "******   ",
+                "**    ** ",
+                "**     **",
+                "**    ** ",
+                "******   ",
+                "**       ",
+                "**       "
+        }),
+        new CharacterPatternMap('S', new String[]{
+                "   ***** ",
+                " **      ",
+                "**       ",
+                "  *****  ",
+                "      ** ",
+                " **   ** ",
+                "  *****  "
+        })
+    };
+
+    for (int row = 0; row < 7; row++) {
+        StringBuilder line = new StringBuilder();
+        for (CharacterPatternMap letter : letters) {
+                letter.getPattern();
+                line.append(letter.getPattern()[row]).append("  ");
+        }
+        System.out.println(line);
+        }
+    }
+}
